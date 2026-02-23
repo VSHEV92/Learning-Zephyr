e761aca46652c17f906ed80b87345f2019346e2b
VSHEV92 <59792635+VSHEV92@users.noreply.github.com>
2026-02-23 22:48:58 +0300

    fix input button example

--- examples/peripheral/input/button/src/main.c
+++ examples/peripheral/input/button/src/main.c
@@ -5,6 +5,7 @@
 
 // get dt nodes base on alises names
 #define LED_NODE DT_ALIAS(led0)
+#define GPIO_KEYS_NODE DT_PATH(gpio_keys)
 
 // get led dt structs
 static const struct led_dt_spec led = LED_DT_SPEC_GET(LED_NODE);
@@ -30,7 +31,7 @@ static void button_cb(struct input_event *evt, void *user_data) {
     - callback function
     - user data pointer
 */
-INPUT_CALLBACK_DEFINE(NULL, button_cb, NULL);
+INPUT_CALLBACK_DEFINE(DEVICE_DT_GET(GPIO_KEYS_NODE), button_cb, NULL);
 
 int main(void) {
 
