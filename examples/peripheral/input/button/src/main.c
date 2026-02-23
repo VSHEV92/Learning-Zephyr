e091a8b6ab3106ca779db233ef4f6cc568536e01
VSHEV92 <59792635+VSHEV92@users.noreply.github.com>
2026-02-23 22:14:10 +0300

    add  input button example

---
+++ examples/peripheral/input/button/src/main.c
@@ -0,0 +1,40 @@
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/led.h>
+#include <zephyr/input/input.h>
+
+
+// get dt nodes base on alises names
+#define LED_NODE DT_ALIAS(led0)
+
+// get led dt structs
+static const struct led_dt_spec led = LED_DT_SPEC_GET(LED_NODE);
+
+// button event callback
+// Get input_event structure 
+//  - device handler, 
+//  - type code: key event, abs value and so on
+//  - sync flag
+//  - key value
+static void button_cb(struct input_event *evt, void *user_data) {
+    if (evt->value) {
+	    led_on_dt(&led);
+    }
+    else {
+	    led_off_dt(&led);
+    }
+}
+
+// set button callback
+/* Parameters:
+    - device, that can trigger callback
+    - callback function
+    - user data pointer
+*/
+INPUT_CALLBACK_DEFINE(NULL, button_cb, NULL);
+
+int main(void) {
+
+	k_sleep(K_FOREVER);
+
+	return 0;
+}
